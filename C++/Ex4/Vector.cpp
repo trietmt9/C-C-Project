d0f90e7edffaa771add7d382b03dbd0730fe52d0
triet <trietmt9@gmail.com>
2024-08-06 11:01:50 +0700

    update vector exercise

--- C++/Ex4/Vector.cpp
+++ C++/Ex4/Vector.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include "vector"
 using namespace std;
-vector <int> test_scores {100, 99, 95, 50, 100};
+vector <int> test_scores;
 
 int main()
 {
     for(int count = 0; count < 5; count++)
     {
-        cin >> test_scores.at(count);
+        int elements;
+        cout << "Insert element: ";
+        cin >> elements;
+        test_scores.push_back(elements);
     }
-    for(int count = 0; count < 5; count ++)
+    
+    for(int count = 0; count < test_scores.size(); count ++)
     {
         cout << "Test score: "<< test_scores.at(count) <<endl;
     }
