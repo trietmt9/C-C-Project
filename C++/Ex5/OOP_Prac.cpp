297c52c28a83ae56ac5d2d362a33164b117dd17c
triet <trietmt9@gmail.com>
2024-08-06 16:32:16 +0700

    OOP private & public training

--- C++/Ex5/OOP_Prac.cpp
+++ C++/Ex5/OOP_Prac.cpp
@@ -9,8 +9,7 @@ class BankAccount
         // Attributes (variables)
         string name; 
         int ID;
-        float Balance;
-    
+
         // Methods (function)
         double deposit(double bal)
         {
@@ -20,6 +19,19 @@ class BankAccount
         {
             return bal;
         }
+        void setBalance(double bal)
+        {
+            Balance = bal; 
+        }
+        double getBalance()
+        {
+            return Balance;
+        }
+    private:
+        // Private attributes
+        float Balance;
+    
+        
 
 };
 
@@ -28,31 +40,29 @@ int main()
     BankAccount myAccount; 
     myAccount.name = "Triet";
     myAccount.ID = 23412;
-    myAccount.Balance = 500.500;
-   
-    cout << "My name is: "           << myAccount.name      << endl;
-    cout << "My account ID is: "     << myAccount.ID        << endl;
-    cout << "My Balance is: "        << myAccount.Balance   << endl;
-    cout << "Deposit: " << myAccount.deposit(1000) << " Withdraw " << myAccount.withDraw(500) << endl;
+    myAccount.setBalance(10000.0);
+    cout << "My name is: "              << myAccount.name                                       << endl;
+    cout << "My account ID is: "        << myAccount.ID                                         << endl;
+    cout << "My current balance is: "   << myAccount.getBalance()                               << endl;
+    cout << "Deposit: " << myAccount.deposit(1000) << " Withdraw "  << myAccount.withDraw(500)  << endl;
 
     BankAccount hisAccount;
     hisAccount.name = "Frank";
     hisAccount.ID = 23413;
-    hisAccount.Balance = 200.500;
-    
-    cout << "\nHis name is: "         <<hisAccount.name     << endl;
-    cout << "His account ID is: "     <<hisAccount.ID       << endl;
-    cout << "His Balance is: "        <<hisAccount.Balance  << endl;
-    cout << "Deposit: " << myAccount.deposit(500) << " Withdraw " << myAccount.withDraw(8000) << endl;
+    hisAccount.setBalance(20000.0);
+    cout << "\nHis name is: "                       << hisAccount.name                           << endl;
+    cout << "His account ID is: "                   << hisAccount.ID                             << endl;
+    cout << "His current balance is: "              << hisAccount.getBalance()                   << endl;
+    cout << "Deposit: " << myAccount.deposit(500)   << " Withdraw " << myAccount.withDraw(8000)  << endl;
 
     BankAccount *newAccount = new BankAccount;
     newAccount->ID = 2321;
     newAccount->name = "Stephen";
-    newAccount->Balance = 100.124;
-    cout << "\nAccount name is: "       <<newAccount->name     << endl;
-    cout << "Account ID is: "           <<newAccount->ID       << endl;
-    cout << "Account Balance is: "      <<newAccount->Balance  << endl;
-    cout << "Deposit: " << myAccount.deposit(5000) << " Withdraw " << myAccount.withDraw(9000) << endl;
+    newAccount->setBalance(30000.0);
+    cout << "\nAccount name is: "               << newAccount->name                               << endl;
+    cout << "Account ID is: "                   << newAccount->ID                                 << endl;
+    cout << "Account current balance is: "      << newAccount->getBalance()                         << endl;
+    cout << "Deposit: " << myAccount.deposit(5000) << " Withdraw " << myAccount.withDraw(9000)    << endl;
 
     delete newAccount;
 }   
