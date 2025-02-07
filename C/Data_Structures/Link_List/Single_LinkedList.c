bcc77b2f810307b2d7c5882b79508cd07a3215f2
trietmt9 <trietmt9@gmail.com>
2025-02-07 17:05:44 +0700

    modified single linked list

--- C/Data_Structures/Link_List/Single_LinkedList.c
+++ C/Data_Structures/Link_List/Single_LinkedList.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-typedef struct node_t {
-    int data;
-    struct node_t *link;
-} node_t;
-
-node_t *create_node(int data) {
-    node_t *new_node = malloc(sizeof(node_t));
-    if (!new_node) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
+struct node
+{
+    uint8_t data; 
+    struct node *link; 
+};
+void create_a_node_begin(struct node* head, uint8_t data)
+{
+    struct node *new_node = (struct node *) malloc(sizeof(struct node));
     new_node->data = data;
+    new_node->link = head->link;
+    head->link = new_node;
+}
+void create_a_node_end(struct node *head, uint8_t data)
+{
+    struct node *new_node = (struct node *) malloc(sizeof(struct node));
+    new_node->data = data; 
     new_node->link = NULL;
-    return new_node;
-}
-
-void print_list(node_t *head) {
-    node_t *current = head;
-    while (current) {
-        printf("Data: %d | Node Address: %p | Next Address: %p\n",
-               current->data, (void *)current, (void *)current->link);
-        current = current->link;
+    struct node * ptr = head; 
+    while(ptr->link !=NULL)
+    {
+        ptr = ptr->link;
     }
+    ptr->link = new_node;
 }
 
-void free_list(node_t *head) {
-    node_t *current = head;
-    while (current) {
-        node_t *temp = current;
+int main()
+{
+    struct node* head = (struct node *) malloc(sizeof(struct node));
+    struct node* current = (struct node*) malloc(sizeof(struct node));
+    head->data = 10;
+    head->link = NULL;
+    create_a_node_end(head, 20);
+    create_a_node_end(head, 30);
+    create_a_node_end(head, 40);
+    create_a_node_end(head, 50);
+    create_a_node_begin(head, 10);
+
+    current = head;
+    while(current!=NULL)
+    {   
+        
+        printf("Data: %d | Current Address: %p| Next Address: %p\n", current->data, current, current->link);
+        // printf("Data: %d | Next Address: %p\n", head->data, head->link);
         current = current->link;
-        free(temp);
+        // head = head->link;
+        
     }
-}
-
-int main() {
-    node_t *head = create_node(10);
-    head->link = create_node(20);
-    head->link->link = create_node(30);
-    head->link->link->link = create_node(40);
-
-    print_list(head);
-    free_list(head);
-
     return 0;
 }
