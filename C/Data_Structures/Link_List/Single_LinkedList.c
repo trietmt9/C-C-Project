f47cc6664db0f02dce96103815fe11e2a8b74fb1
trietmt9 <trietmt9@gmail.com>
2025-02-10 09:06:36 +0700

    adding new API controls for Single LinkedList

--- C/Data_Structures/Link_List/Single_LinkedList.c
+++ C/Data_Structures/Link_List/Single_LinkedList.c
@@ -27,27 +27,72 @@ void create_a_node_end(struct node *head, uint8_t data)
     ptr->link = new_node;
 }
 
+void delete_a_node_begin(struct node *head)
+{
+    struct node *ptr = head->link;
+    head->link = ptr->link;
+    free(ptr);
+}
+
+void delete_list_element(struct node *head, uint8_t data)
+{
+    struct node *ptr = head;
+    struct node *temp = head->link;
+    while(temp!=NULL)
+    {
+        if(temp->data == data)
+        {
+            ptr->link = temp->link;
+            free(temp);
+            break;
+        }
+        ptr = temp;
+        temp = temp->link;
+    }
+}
+
+void display_list(struct node *head)
+{
+    struct node *ptr = head->link;
+    while(ptr!=NULL)
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->link;
+    }
+    if(ptr==NULL)
+    {
+        printf("NULL\n");
+    }
+}
+
+void free_list(struct node *head)
+{
+    struct node *ptr = head->link;
+    while(ptr!=NULL)
+    {
+        struct node *temp = ptr;
+        ptr = ptr->link;
+        free(temp);
+    }
+    head->link = NULL;
+}
+
 int main()
 {
     struct node* head = (struct node *) malloc(sizeof(struct node));
-    struct node* current = (struct node*) malloc(sizeof(struct node));
-    head->data = 10;
+    head->data = 0;
     head->link = NULL;
     create_a_node_end(head, 20);
     create_a_node_end(head, 30);
     create_a_node_end(head, 40);
     create_a_node_end(head, 50);
     create_a_node_begin(head, 10);
-
-    current = head;
-    while(current!=NULL)
-    {   
-        
-        printf("Data: %d | Current Address: %p| Next Address: %p\n", current->data, current, current->link);
-        // printf("Data: %d | Next Address: %p\n", head->data, head->link);
-        current = current->link;
-        // head = head->link;
-        
-    }
+    create_a_node_begin(head, 100);
+    create_a_node_end(head, 60);
+    display_list(head);
+    delete_list_element(head, 40);
+    display_list(head);
+    free_list(head);
+    display_list(head);
     return 0;
 }
